ac42f4a48216c9a8f55b30426365a4a476c3115c
nhutduy-1905 <baominh10cn@gmail.com>
2024-06-08 19:38:04 +0700

    Add files via upload

---
+++ Phantichlop video channel/Phantichlop video channel/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+namespace MyApplication
+{
+    class Video
+    {
+        private String nameChannel;
+        private String avatarChannel;
+        private String timeVideo;
+        private String viewVideo;
+        private String nameVideo;
+        private String avatarVideo;
+        public Video(String newNameChannel, String newAvatarChannel, String newTimeVideo, String newViewVideo, String newNameVideo, String newAvatarVideo)
+        {
+            nameChannel = newNameChannel;
+            avatarChannel = newAvatarChannel;
+            timeVideo = newTimeVideo;
+            viewVideo = newViewVideo;
+            nameVideo = newNameVideo;
+            avatarVideo = newAvatarVideo;
+        }
+        public String getInformationVideo() { return nameChannel + "," + avatarChannel + "," + timeVideo + "," + viewVideo +"," + nameVideo + "," + avatarVideo; }
+        public void setVideoName(String newVideoName)
+        {
+            nameVideo = newVideoName;
+        }
+    class Channel
+        {
+           private String channelName;
+           private String avatarChannel;
+           private String sumNumberSubcriber;
+           private String numberVideo;
+           private String channelIntroduction;
+           private bool subcribe;
+            public Channel(String newChannelName, String newAvatarChannel, String newSumNumberSubcriber, String newNumberVideo, String newChannelIntroduction, bool newSubcribe)
+            {
+                channelName = newChannelName;
+                avatarChannel = newAvatarChannel;
+                sumNumberSubcriber = newSumNumberSubcriber;
+                numberVideo = newNumberVideo;
+                channelIntroduction = newChannelIntroduction;
+                subcribe = newSubcribe;
+            }
+            public String getInformationChannel() { return channelName + "," + avatarChannel + "," + sumNumberSubcriber + "," + numberVideo + "," + channelIntroduction + "," + subcribe; }
+            public void setChannelName(String newChannelName)
+            {
+                channelName = newChannelName;
+            }
+            class Program
+                {
+                static void Main(string[] args)
+                {
+                    Video v = new Video("btrann",
+                        "https://yt3.googleusercontent.com/ANLvpKDqDwu21odl58XTeRGBqMiyMPLHHGLuOakCB4ZNfs_Q-EhE2z2VIta1hTjTGCChR6xWMgY=s160-c-k-c0x00ffffff-no-rj",
+                        "16:04",
+                        "73N",
+                        "[Động lực học tập] Hãy sống đúng với ước mơ của bạn",
+                        "https://youtu.be/brXyzK0yQbU?t=13"
+                        );                  
+                    Console.OutputEncoding = Encoding.UTF8;
+                    Console.WriteLine(v.nameVideo);
+                    Console.WriteLine(v.nameChannel);
+                    Console.WriteLine(v.getInformationVideo());
+                    v.setVideoName("Hãy cố gắng làm tốt nhất những gì bạn có thể");
+                    Console.WriteLine(v.nameVideo);
+                    Channel c = new Channel(
+                        "btrann",
+                        "https://yt3.googleusercontent.com/ANLvpKDqDwu21odl58XTeRGBqMiyMPLHHGLuOakCB4ZNfs_Q-EhE2z2VIta1hTjTGCChR6xWMgY=s160-c-k-c0x00ffffff-no-rj",
+                        "7,95N",
+                        "93 Videos",
+                        "Trạm phát động lực study⏱️📚\r\n💗Đăng kí kênh nhé",
+                        true
+                        );
+                    Console.WriteLine(c.channelName);
+                    Console.WriteLine(c.getInformationChannel());
+                    Console.WriteLine(c.channelName);
+                    c.setChannelName("Phan Hoang Nguyen");
+                    Console.WriteLine(c.channelName);
+                }  
+            }
+         }
+    }
+
+}
